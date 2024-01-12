d607228d087a4f956656394989c72fa1be223229
natal <svetnata74@mail.ru>
2024-01-12 19:07:59 +0100

    dfs

--- tests/test_add_product_to_cart.py
+++ tests/test_add_product_to_cart.py
@@ -2,44 +2,44 @@ import allure
 from metro_tests_suite.pages.product_page import Product
 
 
-@allure.title('Add product to cart (unregistered user)')
-def test_add_product():
-    product = Product()
-
-    with allure.step('Open main page'):
-        product.open_main_page()
-
-    with allure.step('Enter the product name'):
-        product.search_product('METRO Chef Мороженое Пломбир ванильный, 2.5кг')
-
-    with allure.step('Add to cart'):
-        product.add_to_cart()
-
-    with allure.step('Enter the delivery address'):
-        product.enter_the_delivery_address('Санкт-Петербург, Лиговский проспект, 50')
-
-    with allure.step('Click to the cart icon'):
-        product.click_to_the_cart()
-
-    with allure.step('Checkout'):
-        product.click_to_the_checkout_button()
-
-    with allure.step('Assert'):
-        product.assert_checkout()
-
-
-@allure.title('Add product to cart with pickup')
-def test_add_product_to_cart_with_pickup():
-    product = Product()
-
-    with allure.step('Open main page'):
-        product.open_main_page()
-
-    with allure.step('Enter the product name'):
-        product.search_product('METRO Chef Лапша гречневая, 600г')
-
-    with allure.step('Add to cart'):
-        product.add_to_cart()
-
-    with allure.step('Change delivery to pickup'):
-        product.click_to_the_pickup_button()
+# @allure.title('Add product to cart (unregistered user)')
+# def test_add_product():
+#     product = Product()
+#
+#     with allure.step('Open main page'):
+#         product.open_main_page()
+#
+#     with allure.step('Enter the product name'):
+#         product.search_product('METRO Chef Мороженое Пломбир ванильный, 2.5кг')
+#
+#     with allure.step('Add to cart'):
+#         product.add_to_cart()
+#
+#     with allure.step('Enter the delivery address'):
+#         product.enter_the_delivery_address('Санкт-Петербург, Лиговский проспект, 50')
+#
+#     with allure.step('Click to the cart icon'):
+#         product.click_to_the_cart()
+#
+#     with allure.step('Checkout'):
+#         product.click_to_the_checkout_button()
+#
+#     with allure.step('Assert'):
+#         product.assert_checkout()
+#
+#
+# @allure.title('Add product to cart with pickup')
+# def test_add_product_to_cart_with_pickup():
+#     product = Product()
+#
+#     with allure.step('Open main page'):
+#         product.open_main_page()
+#
+#     with allure.step('Enter the product name'):
+#         product.search_product('METRO Chef Лапша гречневая, 600г')
+#
+#     with allure.step('Add to cart'):
+#         product.add_to_cart()
+#
+#     with allure.step('Change delivery to pickup'):
+#         product.click_to_the_pickup_button()
--- tests/test_catalog_section.py
+++ tests/test_catalog_section.py
@@ -2,18 +2,18 @@ import allure
 from metro_tests_suite.pages.catalog_page import Catalog
 
 
-@allure.title('Catalog section')
-def test_simple_catalog_section():
-    catalog_section = Catalog()
-
-    with allure.step('Open main page'):
-        catalog_section.open_main_page()
-
-    with allure.step('Open catalog section'):
-        catalog_section.open_catalog()
-
-    with allure.step('Select the catalog position'):
-        catalog_section.select_the_catalog_position()
-
-    with allure.step('Assert catalog section'):
-        catalog_section.assert_catalog_section()
+# @allure.title('Catalog section')
+# def test_simple_catalog_section():
+#     catalog_section = Catalog()
+#
+#     with allure.step('Open main page'):
+#         catalog_section.open_main_page()
+#
+#     with allure.step('Open catalog section'):
+#         catalog_section.open_catalog()
+#
+#     with allure.step('Select the catalog position'):
+#         catalog_section.select_the_catalog_position()
+#
+#     with allure.step('Assert catalog section'):
+#         catalog_section.assert_catalog_section()
